0a3a9bd626c0f7ffcf394f29f0d1575436ff4a17
pmurph0305 <pmurph0305@gmail.com>
2019-04-01 16:55:41 -0400

    Redux logger & middleware

--- React+Redux+ModuleBundling/Robofriends/src/index.js
+++ React+Redux+ModuleBundling/Robofriends/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import { searchRobotsReducer } from './reducers';
 
+const logger = createLogger();
 
-const store = createStore(searchRobotsReducer);
+const store = createStore(searchRobotsReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}>
                     <App/>
                 </Provider>,
                 document.getElementById('root'));
 registerServiceWorker();
+
