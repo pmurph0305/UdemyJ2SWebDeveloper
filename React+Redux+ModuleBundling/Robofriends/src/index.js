0a21e9753178a9388abf9ab024947231fb6468d4
pmurph0305 <pmurph0305@gmail.com>
2019-04-02 09:57:48 -0400

    Async actions through redux-thunk
    Added redux-thunk, get robot list through an async action using redux-thunk.

--- React+Redux+ModuleBundling/Robofriends/src/index.js
+++ React+Redux+ModuleBundling/Robofriends/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { searchRobotsReducer } from './reducers';
+import { searchRobotsReducer, requestRobotsReducer } from './reducers';
 
 const logger = createLogger();
 
-const store = createStore(searchRobotsReducer, applyMiddleware(logger));
+const rootReducer = combineReducers({searchRobotsReducer, requestRobotsReducer});
+
+const store = createStore(rootReducer, 
+                applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(<Provider store={store}>
                     <App/>
