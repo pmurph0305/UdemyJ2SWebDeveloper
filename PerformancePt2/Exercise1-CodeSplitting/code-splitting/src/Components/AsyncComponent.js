65f05a246f9892cd32a1a5c33b27e3aaeac60acd
pmurph0305 <pmurph0305@gmail.com>
2019-04-05 11:33:55 -0400

    CodeSplitting - Using React.lazy & Suspense

---
+++ PerformancePt2/Exercise1-CodeSplitting/code-splitting/src/Components/AsyncComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default function asyncComponent(importComponent) {
+    class AsyncComponent extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                component: null,
+            }
+        }
+
+        // using async await
+        async componentDidMount() {
+            const { default: component } = await importComponent();
+            this.setState({
+                component: component
+            })
+
+            // Same as...
+            // const component = await importComponent();
+            // this.setState({
+            //     component: component.default
+            // })
+
+        }
+
+        render() {
+            const Component = this.state.component;
+            return Component ? <Component {...this.props}/> : null;
+        }
+    }
+    return AsyncComponent;
+}
