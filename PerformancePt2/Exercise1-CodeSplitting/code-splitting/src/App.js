8b2745a23cdf3b58579df288ab39cf98c1dc5085
pmurph0305 <pmurph0305@gmail.com>
2019-04-05 10:37:01 -0400

    Code-Splitting exercise part 1

---
+++ PerformancePt2/Exercise1-CodeSplitting/code-splitting/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Page1 from './Components/Page1';
+import Page2 from './Components/Page2';
+import Page3 from './Components/Page3';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route})
+  }
+
+  render() {
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange}/>
+    } else if (this.state.route === 'page2') {
+      return <Page2 onRouteChange={this.onRouteChange}/>
+    } else {
+      return <Page3 onRouteChange={this.onRouteChange}/>
+    }
+  }
+}
+
+export default App;
