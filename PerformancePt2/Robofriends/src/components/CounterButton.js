631fd6405f254daaf0e3188e359d37093834af6c
pmurph0305 <pmurph0305@gmail.com>
2019-04-05 13:44:02 -0400

    Progressive Web Apps info

--- PerformancePt2/Robofriends/src/components/CounterButton.js
+++ PerformancePt2/Robofriends/src/components/CounterButton.js
@@ -10,18 +10,18 @@ class CounterButton extends PureComponent {
     }
 
     // Called before render(). Use cautiously, measure performance before using.
-    shouldComponentUpdate(nextProps, nextState) {
-        // console.log("NextProps", nextProps);
-        // console.log("NextState", nextState);
-        // Even though parent is being re-rendered, this would prevent this component from rerendering.
-        // Does essentially the same thing as extending PureComponent.
-        // Except it uses shallow comparison, so it can miss deep nested component updates.
-        if (this.state.count !== nextState.count) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     // console.log("NextProps", nextProps);
+    //     // console.log("NextState", nextState);
+    //     // Even though parent is being re-rendered, this would prevent this component from rerendering.
+    //     // Does essentially the same thing as extending PureComponent.
+    //     // Except it uses shallow comparison, so it can miss deep nested component updates.
+    //     if (this.state.count !== nextState.count) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
     updateCount = () => {
         this.setState(state => { 
