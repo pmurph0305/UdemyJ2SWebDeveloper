04ba496c15a1f069eea44c0886a442e756b5ba4b
pmurph0305 <pmurph0305@gmail.com>
2019-04-09 12:50:52 -0400

    Move app component part to MainPage.js for testing. Add testing MainPage.js

--- Testing/RobofriendsPWA/src/containers/App.js
+++ Testing/RobofriendsPWA/src/containers/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import ErrorBoundary from '../components/ErrorBoundary';
-import Header from '../components/Header';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
+
+import MainPage from '../components/MainPage';
 import './App.css';
 
 import { setSearchField, requestRobots } from '../actions'
 
-const mapStateToProps = state => ({
+ const mapStateToProps = state => ({
 	searchField: state.searchRobotsReducer.searchField,
 
 	robots: state.requestRobotsReducer.robots,
@@ -17,7 +14,7 @@ const mapStateToProps = state => ({
 	isPending: state.requestRobotsReducer.isPending,
 })
 
-const mapDispatchToProps = (dispatch) => {
+ const mapDispatchToProps = (dispatch) => {
 	return { 
 		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
 		onRequestRobots: () => dispatch(requestRobots()),
@@ -25,29 +22,8 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-
-	componentDidMount() {
-		this.props.onRequestRobots();
-	}
-
 	render() {
-		const { isPending, robots, searchField, onSearchChange } = this.props;
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchField.toLowerCase());
-		})
-		return (
-				<div className='tc'>
-					<Header/>
-					<SearchBox searchChange={onSearchChange} />
-					<Scroll>
-					{ isPending ? <h1>Loading...</h1> :
-						<ErrorBoundary>
-							<CardList robots={filteredRobots} />
-						</ErrorBoundary>
-					}
-					</Scroll>
-				</div>
-			)
+		return <MainPage {...this.props}/>
 	}
 }
 
