8840d060d2dc0c5833599db9a1e76b3ef8b9b82d
pmurph0305 <pmurph0305@gmail.com>
2019-04-23 14:48:45 -0400

    Security section complete

--- Security/security-server-exercise/server.js
+++ Security/security-server-exercise/server.js
@@ -2,18 +2,45 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 var winston = require('winston');
+const morgan = require('morgan');
 
 const bodyParser = require('body-parser');
 const app = express()
-app.use(cors())
 app.use(helmet())
+
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// Cors options / configuration.
+// good list of domains to allow to access our endpoints.
+var whitelist = ['null', 'http://example1.com', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => {
+  // http only cookie
+  // res.cookie('session', '1', {httpOnly: true})
+  // // secure cookie
+  // res.cookie('session', '1', { secure: true})
+  // // set content security polcity headers for what scripts to trust.
+  // res.set({
+  //   'Content-Security-Policy': "script-src 'self' https://apis.google.com",
+  // })
+  res.send('Hello World!')
+})
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
-  console.log(userInput);
   if (userInput) {
     winston.log('info', 'user input: ' + userInput);
     res.status(200).json('success');
